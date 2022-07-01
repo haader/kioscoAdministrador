9767f18997a3af314af9c6308f5305e01b87c7dc
[haader] <[37divisas38@gmail.com]>
2022-06-30 23:40:49 -0300

    innit

---
+++ logica.js
@@ -0,0 +1,33 @@
+//https://parzibyte.me/blog/2020/06/22/leer-codigo-barras-javascript-camara/
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	const $resultados = document.querySelector("#resultado");
+	Quagga.init({
+		inputStream: {
+			constraints: {
+				width: 960,
+				height: 540,
+			},
+			name: "Live",
+			type: "LiveStream",
+			target: document.querySelector('#contenedor'), // Pasar el elemento del DOM
+		},
+		decoder: {
+			readers: ["ean_reader"]
+		}
+	}, function (err) {
+		if (err) {
+			console.log(err);
+			return
+		}
+		console.log("Iniciado correctamente");
+		Quagga.start();
+	});
+
+	Quagga.onDetected((data) => {
+		$resultados.textContent = data.codeResult.code;
+		// Imprimimos todo el data para que puedas depurar
+		console.log(data);
+	});
+});
