865394de7fcbe9930c7442bf67dccbd7048c3e45
[haader] <[37divisas38@gmail.com]>
2022-07-01 00:15:08 -0300

    barra

--- logica.js
+++ logica.js
@@ -1,13 +1,11 @@
 //https://parzibyte.me/blog/2020/06/22/leer-codigo-barras-javascript-camara/
-
-
 document.addEventListener("DOMContentLoaded", () => {
 	const $resultados = document.querySelector("#resultado");
 	Quagga.init({
 		inputStream: {
 			constraints: {
-				width: 960,
-				height: 540,
+				width: 1920,
+				height: 1080,
 			},
 			name: "Live",
 			type: "LiveStream",
@@ -18,16 +16,40 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	}, function (err) {
 		if (err) {
-			alert(err);
+			console.log(err);
 			return
 		}
-		alert("Iniciado correctamente");
+		console.log("Iniciado correctamente");
 		Quagga.start();
 	});
 
 	Quagga.onDetected((data) => {
 		$resultados.textContent = data.codeResult.code;
 		// Imprimimos todo el data para que puedas depurar
-		alert(data);
+		console.log(data);
+	});
+
+	Quagga.onProcessed(function (result) {
+		var drawingCtx = Quagga.canvas.ctx.overlay,
+			drawingCanvas = Quagga.canvas.dom.overlay;
+
+		if (result) {
+			if (result.boxes) {
+				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+				result.boxes.filter(function (box) {
+					return box !== result.box;
+				}).forEach(function (box) {
+					Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+				});
+			}
+
+			if (result.box) {
+				Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+			}
+
+			if (result.codeResult && result.codeResult.code) {
+				Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+			}
+		}
 	});
 });
